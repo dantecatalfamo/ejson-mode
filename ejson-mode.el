af393675ab811472b18ffee024ac7dbfa5226baf
Dante Catalfamo <dante.catalfamo@shopify.com>
2019-06-06 22:41:53 -0400

    Bugfix version bump

--- ejson-mode.el
+++ ejson-mode.el
@@ -1,7 +1,7 @@
 ;;; ejson-mode.el --- Major Mode for editing ejson files. -*- lexical-binding: t -*-
 
 ;; URL: https://github.com/dantecatalfamo/ejson-mode
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requies: ((emacs "24"))
 
 ;;; Commentary:
